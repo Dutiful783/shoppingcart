f7870e8c97fdf78b2d4dbecc693f0466c793dacc
PiusGodwin <dutifulgodwin2020@gmail.com>
2023-03-24 22:58:10 -0700

    commit

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import Product1 from './assets/products/1.png';
+import Product2 from './assets/products/2.png';
+import Product3 from './assets/products/3.png';
+import Product4 from './assets/products/4.png';
+import Product5 from './assets/products/5.png';
+import Product6 from './assets/products/6.webp';
+import Product7 from './assets/products/7.webp';
+import Product8 from './assets/products/8.webp';
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "iPhone",
+        price: 999.0,
+        productImage: Product1
+    },
+    {
+        id: 2,
+        productName: "Macbook Pro 2022 (M1)",
+        price: 1999.0,
+        productImage: Product2,
+    },
+    {
+        id: 3,
+        productName: "Cannon M50 Camera",
+        price: 699.0,
+        productImage: Product3,
+    },
+    {
+        id: 4,
+        productName: "WLS Van Gogh Denim Jacket",
+        price: 228.0,
+        productImage: Product4,
+    },
+    {
+        id: 5,
+        productName: "LED Light Strips",
+        price: 19.99,
+        productImage: Product5,
+    },
+    {
+        id: 6,
+        productName: "SPECTRUM LS TEE",
+        price: 68.0,
+        productImage: Product6,
+    },
+    {
+        id: 7,
+        productName: "AUTO SERVICE SHIRT by GOLF WANG",
+        price: 120.0,
+        productImage: Product7,
+    },
+    {
+        id: 8,
+        productName: "DON'T TRIP UNSTRUCTURED HAT",
+        price: 40.0,
+        productImage: Product8,
+    }
+];
